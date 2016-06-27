8b873e6f0e1f4dba48b60dea92ccd489eb4ec17c
klishevich <busation@gmail.com>
2016-06-27 08:44:54 +0300

    Add new list API

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,7 +4,29 @@ class ListsController < ApplicationController
   	render json: @lists
   end
 
+  def new
+    @list = List.new
+  end
+
+  # def create
+  #   @category = Category.new(list_params)
+  #   if @category.save
+  #     flash[:success] = t(:saved_successfuly)
+  #     redirect_to edit_category_path @category
+  #   else
+  #     render 'new'
+  #   end  
+  # end
+
   def create
+    @list = List.new(list_params)
+    if @list.save
+      @lists = List.all
+    else
+      #TODO: add errors
+      @lists = List.all
+    end
+    render json: @lists
   end
 
   def update
@@ -12,4 +34,9 @@ class ListsController < ApplicationController
 
   def delete
   end
+
+  def list_params
+    params.require(:list).permit(:name, :order)
+  end 
+
 end
